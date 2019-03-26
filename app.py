8461466bf74b42a7ac110800853b95ff84a4774e
eduardowt <jewt_5@hotmail.com>
2019-03-26 21:59:11 +0000

    added logging, waitress eats the rest

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
+
+#CONFIGURING LOGGING
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fh = logging.FileHandler('mylogs.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 
 
 @app.route('/')
@@ -8,5 +25,8 @@ def homepage():
     return "Hello World"
 
 
+logger.info('STARTING APP, TRY IT OUT!!!')
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
