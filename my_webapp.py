2fb56cc26ca542e95e8bb0fae9c5514c31892f10
DeerFriend <yaseminshn@gmail.com>
2019-03-27 20:26:13 +0000

    mistake_corrected

--- my_webapp.py
+++ my_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
 
 flask_app = Flask(__name__)
@@ -18,10 +18,9 @@ fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-@app.route("/<name>")
+@flask_app.route("/<name>")
 def hello_someone(name):
-return render_template("hello.html", name=name.title())
-
+	return render_template("hello.html", name=name.title())
 
 @flask_app.route('/')
 def homepage():
