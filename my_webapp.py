00be267e3ec7b2d7d257bc0b07a6dda4dd196d17
LauraFayTurner <laurafayturner@gmail.com>
2019-03-27 20:26:34 +0000

    a new decorator

--- my_webapp.py
+++ my_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
 
 flask_app = Flask(__name__)
@@ -18,15 +18,15 @@ fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-
-
 @flask_app.route('/')
 def homepage():
     return "Hello World"
 
+@flask_app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
 
 logger.info('STARTING APP, TRY IT OUT!!!')
 
 if __name__ == '__main__':
     flask_app.run(debug=True, use_reloader=True)
-
