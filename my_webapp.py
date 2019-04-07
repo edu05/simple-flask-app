4e36073c9a8b4b63329858aef53a19fa2dad98d6
vodriguez <agarciarod@gmail.com>
2019-04-07 14:18:14 +0100

    trying to sort out mail send

--- my_webapp.py
+++ my_webapp.py
@@ -42,3 +42,31 @@ logger.info('STARTING APP, TRY IT OUT!!!')
 
 if __name__ == '__main__':
     flask_app.run(debug=True, use_reloader=True)
+
+
+
+import requests
+
+sender = ["Creative Aspirations"]
+domain_name = "sandboxb49a93eedf4144a9ac2ed0a12ed78f79.mailgun.org"
+api_key = "e201eb040a4d63c5f95987aa75ae992a-de7062c6-3b003f5d"
+email_address = request.forms.get('email_address')
+
+def send_simple_message():
+    return requests.post(
+        "https://api.mailgun.net/v3/{0}/messages".format(domain_name),
+        auth=("api", api_key),
+        data={
+            "from": "{0} <{0}@{1}>".format(sender[1], domain_name),
+            "to": ["{0}"].format(email_address),
+            "subject": "Thanks for signing up!",
+            "text": "Thanks for signing up to Creative Aspirations! We'll be sure to keep you updated according to your preferences."
+            }
+        )
+
+response = send_simple_message()
+
+print response.url
+print response.status_code
+print response.headers["content-type"]
+print response.text
